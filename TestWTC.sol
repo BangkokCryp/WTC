4f5e0ef29481368f13d77bad8de1f95acf4dcf21
Katawut Paramee <katawut@mail.com>
2023-01-28 22:46:49 +0700

    Update TestWTC.sol
    === Original File === 
    
    KAP20.txt from https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbkpQZ2pIMEhQTWJIbVhfajFYSEhqZU5ndjN1d3xBQ3Jtc0ttMF9kbTczb0lsV1hpUGRMejNVS2psc2RNSW5GU1g1bm5zS21VS183cGxxTy1QUF9uSzBYUlNMbEs1WHRHVHd6ZXFnOWdmQy1xNUlwdnZmV0hvS0JBOXpCWjdLSTk4OElQVlVwQnBHY1NpRTZ4Mnl1Yw&q=https%3A%2F%2Fwww.mediafire.com%2Ffile%2F5ao61veaa6iv79z%2FKAP20.txt%2Ffile&v=bCrZxEGrxT8
    
    Refer to Tutorial Channel: TechLearning (https://www.youtube.com/@techlearning3468)
    Tutorial Link: https://www.youtube.com/watch?v=bCrZxEGrxT8

--- TestWTC.sol
+++ TestWTC.sol
@@ -1,215 +1,91 @@
-/**
- *Submitted for verification at Etherscan.io on 2019-02-11
-*/
+pragma solidity ^0.5.0;
 
-pragma solidity >=0.4.22 <0.6.0;
+// ----------------------------------------------------------------------------
+// KAP Token Standard #20 Interface
+//
+// ----------------------------------------------------------------------------
+contract KAP20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-contract owned {
-    address public owner;
-    address public manager;
-    address public operation;
-    address public miner;
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
-    constructor() public {
-        owner = msg.sender;
+// ----------------------------------------------------------------------------
+// Safe Math Library 
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
     }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-    
-    modifier onlyManager {
-        require(msg.sender == manager);
-        _;
-    }
-    
-    modifier onlyOperation {
-        require(msg.sender == operation || msg.sender == manager);
-        _;
-    }
-    
-    modifier onlyMiner {
-        require(msg.sender == miner);
-        _;
-    }
-    
-    modifier onlyOwnerAndManager {
-        require(msg.sender == owner || msg.sender == manager);
-        _;
-    }
-    
-    modifier onlyManagerAndOperation {
-        require(msg.sender == operation || msg.sender == manager);
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
-    }
-    
-    function setManager(address newManager) onlyOwnerAndManager public {
-        manager = newManager;
-    }
-    
-    function setOperation(address newOperation) onlyOwnerAndManager public {
-        operation = newOperation;
-    }
-    
-    function setMiner(address newMiner) onlyOwnerAndManager public {
-        miner = newMiner;
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0);
+        c = a / b;
     }
 }
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; }
 
-contract TokenERC20 {
-
+contract SmokingToken is KAP20Interface, SafeMath {
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
-    uint256 public totalSupply;
-    uint256 public supplyLimit;
-
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    event Mint(address indexed from, address indexed to, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event Burn(address indexed from, uint256 value);
-
+    uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
+    
+    uint256 public _totalSupply;
+    
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+    
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
     constructor() public {
-        totalSupply = 0;
-        supplyLimit = 0;
-        name = 'Bitkub Token';
-        symbol = 'KUB';
-    }
-
-    function _transfer(address _from, address _to, uint _value) internal {
-        require(_to != address(0x0));
-        require(balanceOf[_from] >= _value);
-        require(balanceOf[_to] + _value > balanceOf[_to]);
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);
-        allowance[_from][msg.sender] -= _value;
-        _transfer(_from, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, address(this), _extraData);
-            return true;
-        }
-    }
-
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        totalSupply -= _value;
-        emit Burn(msg.sender, _value);
-        return true;
-    }
-
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
-        allowance[_from][msg.sender] -= _value;
-        totalSupply -= _value;
-        emit Burn(_from, _value);
-        return true;
-    }
-}
-
-
-contract Token is owned, TokenERC20 {
-    string public detail;
-    string public website;
-    address public dapp;
-
-    mapping (address => bool) public frozenAccount;
-
-    event FrozenFunds(address target, bool frozen);
-    event SetSupply(uint256 value, string note);
-    event BurnDirect(address indexed from, uint256 value, string note);
-
-    constructor() TokenERC20() public {}
-
-    function setDetail(string memory newDetail, string memory newWebsite) onlyOwnerAndManager public {
-        detail = newDetail;
-        website = newWebsite;
+        name = "SmokingToken";
+        symbol = "SMOKE";
+        decimals = 18;
+        _totalSupply = 100000000000000000000000000;
+        
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
     
-    function setSupply(uint _value,string memory _note) onlyOwnerAndManager public {
-        require (totalSupply <= _value);
-        supplyLimit = _value;
-        emit SetSupply(_value, _note);
+    function totalSupply() public view returns (uint) {
+        return _totalSupply  - balances[address(0)];
     }
     
-    function setDapp(address _address) onlyOwnerAndManager public {
-        dapp = _address;
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
     }
     
-    function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != address(0x0));
-        require (balanceOf[_from] >= _value);
-        require (balanceOf[_to] + _value >= balanceOf[_to]);
-        require(!frozenAccount[_from]);
-        require(!frozenAccount[_to]);
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
     }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        _transfer(msg.sender, _to, _value);
+    
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }
-
-    function freezeAccount(address _target, bool _freeze) onlyManagerAndOperation public {
-        frozenAccount[_target] = _freeze;
-        emit FrozenFunds(_target, _freeze);
+    
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
     }
-
-    function mintToken(address _target, uint _value) onlyMiner public {
-        require (_target != address(0x0));
-        require (totalSupply <= supplyLimit);
-        balanceOf[_target] += _value;
-        totalSupply += _value;
-        emit Transfer(address(0), address(this), _value);
-        emit Transfer(address(this), _target, _value);
+    
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
     }
-
-    function directBurn(address _from, uint _value,string memory _note) onlyMiner public{
-        require (_from != address(0x0));
-        require(balanceOf[_from] >= _value );
-        balanceOf[_from] -= _value;
-        totalSupply -= _value;
-        emit BurnDirect(_from, _value, _note);
-    }
-
 }
