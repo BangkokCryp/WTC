20cf3b688c039909602d6b8bb5864dc36b073fe9
Katawut Paramee <katawut@mail.com>
2023-01-28 23:37:07 +0700

    Update TestWTC.sol
    Successful Compile TestWTC on BitkubChain TestNet (2023.01.28) at 大谷地

--- TestWTC.sol
+++ TestWTC.sol
@@ -31,7 +31,7 @@ contract SafeMath {
 }
 
 
-contract TestWTC Token is KAP20Interface, SafeMath {
+contract TestWTC is KAP20Interface, SafeMath {
     string public name;
     string public symbol;
     uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
